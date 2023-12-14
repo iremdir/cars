a8c0968add72e9c3d3db10ad9993129865185e95
0irem0 <iremmozkaynak@gmail.com>
2023-12-14 18:49:02 +0300

    finish

--- car.dart
+++ car.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 void main() {
   car carA = car('Bugatti', 'Veyron', 2005, 4000000);
   car carB = car('Lamborghini', 'Veneno', 2013, 4300000);
@@ -14,12 +12,12 @@ void main() {
   car oldest = cars[0];
 
   priceController() {
-    for (car oAnkiCar in cars) {
-      if (oAnkiCar.fiyat >= mostExpensive.fiyat) {
-        mostExpensive = oAnkiCar;
+    for (car i in cars) {
+      if (i.fiyat >= mostExpensive.fiyat) {
+        mostExpensive = i;
       }
-      if (oAnkiCar.fiyat <= cheapest.fiyat) {
-        cheapest = oAnkiCar;
+      if (i.fiyat <= cheapest.fiyat) {
+        cheapest = i;
       }
     }
     print(
@@ -30,10 +28,10 @@ void main() {
 
   ageController() {
     for (car i in cars) {
-      if (i.yil > youngest.fiyat) {
+      if (i.yil > youngest.yil) {
         youngest = i;
       }
-      if (i.yil < oldest.fiyat) {
+      if (i.yil < oldest.yil) {
         oldest = i;
       }
     }
@@ -49,6 +47,9 @@ void main() {
   for (int i = 0; i <= 3; i++) {
     cars[i].fiyat -= cars[i].yil;
   }
+  for (int i = 0; i < cars.length; i++) {
+    print(cars[i]);
+  }
 }
 
 class car {
@@ -57,6 +58,11 @@ class car {
   int yil;
   int fiyat;
   car(this.marka, this.model, this.yil, this.fiyat);
+
+  @override
+  String toString() {
+    return 'markası: $marka , modeli: $model , üretim yılı : $yil , fiyatı : $fiyat';
+  }
 
   int indirim(int tutar) {
     fiyat -= tutar;
